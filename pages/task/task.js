85056ae574b07718d179d9ec27995ca733eee809
梁通达 <1466335800@qq.com>
2023-04-09 17:36:19 +0800

    任务大厅页面初步布局完成

--- pages/task/task.js
+++ pages/task/task.js
@@ -1,66 +1,95 @@
-// pages/task/task.js
+let _this;
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    chooseTab: "task",
+    currentIndex: 0,
+    taskTabTitle: [
+        {index: 0, title: '全部'},
+        {index: 1, title: '代取快递'},
+        {index: 2, title: '代寄快递'},
+        {index: 3, title: '代拿快递'},
+        {index: 4, title: '代购'},
+        {index: 5, title: '代送'},
+        {index: 6, title: '其他'},
+    ],
+    mytaskTabTitle: [
+      {id: 0, title: '全部'},
+      {id: 1, title: '进行中'},
+      {id: 2, title: '待完成'},
+      {id: 3, title: '待评价'}
+    ]
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    _this = this;
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+  /**
+   * 切换顶部 Tab 标签
+   */
+  changTab: function (e) {
+    let name = e.currentTarget.dataset.name;
+    // console.log(name)
+    if (name == "task") {
+      this.setData({
+        chooseTab: name,
+      });
+    } else {
+      this.setData({
+        chooseTab: name,
+      });
     }
-})
+  },
+
+  /**
+   * 子标签切换
+   */
+  taskSwitch: function(e) {
+    // console.log(e.currentTarget.dataset.id)
+    let id = e.currentTarget.dataset.id
+    this.setData({
+      currentIndex: id
+    })
+  },
+});
--- pages/user/apply/apply.js
+++ pages/user/apply/apply.js
@@ -1,68 +1,112 @@
-// pages/user/apply/apply.js
+let _this;
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cert: "",
+    stu_card: "",
+    show: true, // todo 判断当前的认证状态
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        cert: '',
-        stu_card: '',
-        show: true,  // todo 判断当前的认证状态
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    _this = this;
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
 
-    },
+  /**
+   * 用户上传校园卡、身份证
+   */
+  choose: function (e) {
+    let name = e.currentTarget.dataset.name;
+    wx.chooseMedia({
+      count: 1, //一次性最多选取三张
+      mediaType: "image",
+      sourceType: ["album", "camera"],
+      camera: "back",
+      success(res) {
+        // console.log(res.tempFiles.tempFilePath)
+        const tempFilePaths = res.tempFiles;
+        if (name == "cert") {
+          _this.setData({
+            cert: tempFilePaths[0].tempFilePath,
+          });
+        } else {
+          _this.setData({
+            stu_card: tempFilePaths[0].tempFilePath,
+          });
+        }
+      },
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+  /**
+   * 表单上传认证
+   */
+  formSubmit: function (e) {
+    let value = e.detail.value;
+    if (value.name == "") {
+      wx.showToast({
+        title: "请输入姓名",
+        icon: "none",
+      });
+    } else if (value.stu_num == "") {
+      wx.showToast({
+        title: "请输入学号",
+        icon: "none",
+      });
+    } else if (value.card_num == "") {
+      wx.showToast({
+        title: "请输入身份证号",
+        icon: "none",
+      });
+    } else if (this.data.stu_card == "") {
+      wx.showToast({
+        title: "请上传校园卡",
+        icon: "none",
+      });
+    } else if (this.data.cert == "") {
+      wx.showToast({
+        title: "请上传身份证",
+        icon: "none",
+      });
     }
-})
+  },
+});
